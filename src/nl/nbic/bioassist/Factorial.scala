75b99e55d9061a6907a46eb94072cf93f445c01b
Freek de Bruijn <freekdb@gmail.com>
2014-02-03 16:23:35 +0100

    Everyone likes 42

--- src/nl/nbic/bioassist/Factorial.scala
+++ src/nl/nbic/bioassist/Factorial.scala
@@ -4,6 +4,7 @@ object Factorial extends App {
   def factorial(n: BigInt): BigInt =
     if (n <= 0) 1 else n * factorial(n - 1)
 
+  // Everyone likes 42.
   val n = 42
   println(n + "! = " + factorial(n))
 }
